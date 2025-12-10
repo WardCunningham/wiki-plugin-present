0f4e00684b8d5d83d231ecdcd9f61e44995e5ef4
Paul Rodwell <paul.rodwell@btinternet.com>
2025-12-10 11:53:53 +0000

    Migrate test suite to modern JS
    Soft code whispers change
    Syntax shifts like autumn leaves
    Tests bloom, fresh and clean

--- test/test.js
+++ test/test.js
@@ -1,43 +1,44 @@
-# build time tests for present plugin
-# see http://mochajs.org/
+// build time tests for present plugin
+// see http://mochajs.org/
 
-present = require '../client/present'
-expect = require 'expect.js'
+import { present } from '../src/client/present.js'
+import expect from 'expect.js'
 
-describe 'present plugin', ->
-
-  describe 'expand', ->
-
-    it 'can make itallic', ->
-      result = present.expand 'hello *world*'
-      expect(result).to.be 'hello <i>world</i>'
-
-  describe 'elapsed', ->
-
-    it 'can do sub-minute', ->
-      result = present.elapsed 10*1000
-      expect(result).to.be 'a minute ago'
-
-    it 'can do minutes', ->
-      result = present.elapsed 10*60*1000
-      expect(result).to.be '10 minutes ago'
-
-    it 'can do hours', ->
-      result = present.elapsed 10*60*60*1000
-      expect(result).to.be '10 hours ago'
-
-    it 'can do days', ->
-      result = present.elapsed 10*24*60*60*1000
-      expect(result).to.be '10 days ago'
-
-    it 'can do weeks', ->
-      result = present.elapsed 3*7*24*60*60*1000
-      expect(result).to.be '3 weeks ago'
-
-    it 'can do months', ->
-      result = present.elapsed 10*30*24*60*60*1000
-      expect(result).to.be '10 months ago'
-
-    it 'can do years', ->
-      result = present.elapsed 10*365*24*60*60*1000
-      expect(result).to.be '10 years ago'
+describe('present plugin', () => {
+  describe('expand', () => {
+    it('can make itallic', () => {
+      const result = present.expand('hello *world*')
+      expect(result).to.be('hello <i>world</i>')
+    })
+  })
+  describe('elapsed', () => {
+    it('can do sub-minute', () => {
+      const result = present.elapsed(10 * 1000)
+      expect(result).to.be('a minute ago')
+    })
+    it('can do minutes', () => {
+      const result = present.elapsed(10 * 60 * 1000)
+      expect(result).to.be('10 minutes ago')
+    })
+    it('can do hours', () => {
+      const result = present.elapsed(10 * 60 * 60 * 1000)
+      expect(result).to.be('10 hours ago')
+    })
+    it('can do days', () => {
+      const result = present.elapsed(10 * 24 * 60 * 60 * 1000)
+      expect(result).to.be('10 days ago')
+    })
+    it('can do weeks', () => {
+      const result = present.elapsed(3 * 7 * 24 * 60 * 60 * 1000)
+      expect(result).to.be('3 weeks ago')
+    })
+    it('can do months', () => {
+      const result = present.elapsed(10 * 30 * 24 * 60 * 60 * 1000)
+      expect(result).to.be('10 months ago')
+    })
+    it('can do years', () => {
+      const result = present.elapsed(10 * 365 * 24 * 60 * 60 * 1000)
+      expect(result).to.be('10 years ago')
+    })
+  })
+})
